030ecefe308dd8a8e1b0c653bb17d15346893ae6
Rafaelmllv <rafaelmllv@gmail.com>
2022-04-10 00:20:11 -0300

    atualizando repositorio

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="python-pacotes-processamento-imagens",
+    name="image-processing-rafael",
     version="0.0.1",
-    author="Jefferson",
-    author_email="jeffersonknopf97@gmail.com",
-    description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp Cognizant Cloud Data Engineer #2. E-mail:karinatkato@gmail.com.",
+    author="Rafael Vieira",
+    author_email="rafaelmllv@gmail.com",
+    description="Pacote desenvolvido pela professora Karina Kato, para processamento de imagens e demonstração da criação de um pacote python",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeffersonk97/python-pacotes-processamento-imagens",
